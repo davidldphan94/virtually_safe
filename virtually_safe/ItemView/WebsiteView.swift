bb37c1020500e9e22c541656268d7c7c5ec149a9
k-kha <kevinkha7@gmail.com>
2021-05-11 22:56:32 -0400

    Added WebsiteView

---
+++ virtually_safe/ItemView/WebsiteView.swift
@@ -0,0 +1,20 @@
+//
+//  WebsiteView.swift
+//  virtually_safe
+//
+//  Created by Kevin Kha on 5/11/21.
+//
+
+import SwiftUI
+
+struct WebsiteView: View {
+    var body: some View {
+        Text("Website")
+    }
+}
+
+struct WebsiteView_Previews: PreviewProvider {
+    static var previews: some View {
+        WebsiteView()
+    }
+}
