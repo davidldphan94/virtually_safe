4dbf256e2a6f2adca841b364da2b61a97275cbc0
k-kha <kevinkha7@gmail.com>
2021-05-11 23:19:59 -0400

    Added UserView/LoginView.swift

---
+++ virtually_safe/UserView/LoginView.swift
@@ -0,0 +1,117 @@
+//
+//  LoginView.swift
+//  virtually_safe
+//
+//  Created by Kevin Kha on 5/11/21.
+//
+
+import SwiftUI
+import Firebase
+import FirebaseAuth
+
+class userInfo: ObservableObject{
+    @Published var email: String = ""
+}
+
+struct LoginView: View {
+    @ObservedObject var info = userInfo()
+    //@ObservedObject var email: userInfo
+    @State var email = ""
+    @State var password = ""
+    @State var loginSucc = false
+    @State private var loginAlert = false
+    //@ObservedObject var email = ""
+    
+    var alert: Alert {
+        Alert(title: Text("Incorrect Credentials"),
+              message: Text("The username or password is incorrect."))
+    }
+    
+    var body: some View {
+        NavigationView{
+            Group {
+                /*
+                if (loginSucc == true) {
+                    MovieList()
+                } else {*/
+                    VStack {
+                        
+                        Text("Movie Review")
+                            .font(.title)
+                            .bold()
+                        Spacer()
+                        TextField("Email", text: $email)
+                            .font(.system(size: 20))
+                            .padding(10)
+                            //.background(Color.white)
+                            .background(RoundedRectangle(cornerRadius: 10).strokeBorder(Color.blue, lineWidth: 1))
+                            //.cornerRadius(5)
+                            //.padding()
+                        SecureField("Password", text: $password)
+                            .font(.system(size: 20))
+                            .padding(10)
+                            //.background(Color.white)
+                            .background(RoundedRectangle(cornerRadius: 10).strokeBorder(Color.blue, lineWidth: 1))
+                            //.cornerRadius(5)
+                            //.padding()
+                        ZStack {
+                            Button (action: { login() }){
+                            Text("Sign in")
+                            }.alert(isPresented: $loginAlert,
+                                    content: { self.alert })
+                            .frame(width:100, height: 30, alignment: .center)
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                        }
+                        //Spacer()
+                        
+                        HStack {
+                            Text("Don't have an account?")
+                            NavigationLink(destination: WebsiteView(), label: {
+                                Text("Register")
+                                    .bold()
+                                    .foregroundColor(.blue)
+                            })
+                        }
+                        .padding(.top, 50)
+                        Spacer()
+                        
+                        
+                    }
+                    .padding()
+                //}
+            }
+        }
+        .phoneOnlyStackNavigationView().navigationBarBackButtonHidden(true)
+    }
+    
+    func login(){
+        //Auth.auth
+        
+        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+            if error != nil {
+                print(error?.localizedDescription ?? "")
+                loginAlert = true
+            } else {
+                print("Success")
+                loginSucc = true
+            }
+        }
+    }
+}
+
+extension View {
+    func phoneOnlyStackNavigationView() -> some View {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return AnyView(self.navigationViewStyle(StackNavigationViewStyle()))
+        } else {
+            return AnyView(self)
+        }
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
