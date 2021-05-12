e52ce0d0104b46f3929c803b16b780acd23c584a
davidldphan94 <davidldphan94@gmail.com>
2021-05-12 19:19:29 -0400

    Generate Password View

--- virtually_safe/GeneratePasswordView.swift
+++ virtually_safe/GeneratePasswordView.swift
@@ -8,13 +8,53 @@
 import SwiftUI
 
 struct GeneratePasswordView: View {
+    @State var pw_display = "Generate Your Password"
+    @State var uppercase = false
+    @State var lowercase = false
+    @State var numbers = false
+    @State var special_characters = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack() {
+            HStack {
+                Button(action: {}) {
+                    Text("Clear")
+                }.padding()
+                Button(action: {}) {
+                    Text("Copy")
+                }.padding()
+                Button(action: {}) {
+                    Text("History")
+                }.padding()
+            }
+            HStack {
+                Text(pw_display).italic().padding()
+            }.border(Color.black)
+            HStack {
+                Text("Uppercase")
+                
+            }
+            HStack {
+                Text("Lowercase")
+            }
+            HStack {
+                Text("Numbers")
+            }
+            HStack {
+                Text("Special Characters")
+            }
+            Spacer()
+            
+            }.navigationTitle("Generate Password")
+        }
     }
 }
 
 struct GeneratePasswordView_Previews: PreviewProvider {
     static var previews: some View {
-        GeneratePasswordView()
+        Group {
+            GeneratePasswordView()
+        }
     }
 }
