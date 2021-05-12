efe70271120a92c7c20182008983f9ff3486dffc
davidldphan94 <davidldphan94@gmail.com>
2021-05-12 19:07:21 -0400

    storyboard launchscreen

---
+++ virtually_safe/GeneratePasswordView.swift
@@ -0,0 +1,20 @@
+//
+//  GeneratePasswordView.swift
+//  virtually_safe
+//
+//  Created by David Phan on 5/11/21.
+//
+
+import SwiftUI
+
+struct GeneratePasswordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GeneratePasswordView_Previews: PreviewProvider {
+    static var previews: some View {
+        GeneratePasswordView()
+    }
+}
