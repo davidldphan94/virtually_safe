1c7cefcca0e717c59e986e007248b68531ec3df4
Kevin Kha <48115819+k-kha@users.noreply.github.com>
2021-05-12 23:00:39 -0400

    Update WebsiteView.swift

--- virtually_safe/ItemView/WebsiteView.swift
+++ virtually_safe/ItemView/WebsiteView.swift
@@ -8,8 +8,72 @@
 import SwiftUI
 
 struct WebsiteView: View {
+    
     var body: some View {
-        Text("Website")
+        
+        VStack{
+            HStack(alignment: .center){
+                Spacer()
+                Text("Password")
+                    .font(.title)
+                    .bold()
+                    .padding(.leading, 20)
+                Spacer()
+                /*
+                NavigationLink(destination: WebsiteViewEdit()){
+                Text("Edit")
+                }
+                .padding(.trailing, 20)
+                */
+                Text("Edit")
+                    .padding(.trailing, 20)
+            }.padding(.bottom, 30)
+            
+            ScrollView{
+                VStack(alignment: .leading){
+                    HStack{Spacer()}
+                    
+                    Text("Website Name").foregroundColor(.gray).font(.headline)
+                    Text("name")
+                    Divider()
+                    Text("URL").foregroundColor(.gray).font(.headline)
+                    Text("url")
+                    Divider()
+                    Text("Username").foregroundColor(.gray).font(.headline)
+                    Text("user")
+                    
+                }.padding(.leading, 20).padding(.trailing, 20)
+                VStack(alignment: .leading){
+                    HStack{Spacer()}
+                    Divider()
+                    Text("Password").foregroundColor(.gray).font(.headline)
+                    Text("pass")
+                    Divider()
+                    Text("Notes").foregroundColor(.gray).font(.headline)
+                    Text("haha")
+                    
+                }.padding(.leading, 20).padding(.trailing, 20)
+            }
+            
+            Spacer()
+            Divider()
+            HStack{
+                Image(systemName: "key.fill").resizable().frame(width: 20, height: 30)
+                    .foregroundColor(.black)
+                    .padding(.leading, 40)
+                    .padding(.trailing, 40)
+                Divider()
+                Image(systemName: "lock.rotation").resizable().frame(width: 30, height: 30)
+                    .foregroundColor(.gray)
+                    .padding(.leading, 40)
+                    .padding(.trailing, 40)
+                Divider()
+                Image(systemName: "person.crop.circle").resizable().frame(width: 30, height: 30)
+                    .foregroundColor(.gray)
+                    .padding(.leading, 40)
+                    .padding(.trailing, 40)
+            }.frame(width: 350, height: 40, alignment: .center)
+        }
     }
 }
 
