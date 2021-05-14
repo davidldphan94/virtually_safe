f6077e8006eb865ade5ad34013e257fffb386e4f
davidldphan94 <davidldphan94@gmail.com>
2021-05-14 02:52:59 -0400

    removed typo from PasswordHistory model

--- virtually_safe/Model/PasswordHistory.swift
+++ virtually_safe/Model/PasswordHistory.swift
@@ -8,31 +8,8 @@
 import Foundation
 import SwiftUI
 
-struct MovieResults: Decodable {
-    let page: Int?
-    let total_pages: Int?
-    let total_results: Int?
-    var results: [Movie]
+class PasswordHistory : ObservableObject {
+    @Published var passwords = [String]()
+    @Published var dates = [String]()
 }
-
-struct Movie: Hashable, Decodable, Identifiable {
-    var id: Int
-    var vote_count: Int
-    var vote_average: Double
-    var title: String
-    var backdrop_path: String
-    var poster_path: String
-    var release_date: String
-    var overview: String
-    var genre_ids: [Int]
-        
-    var backdropURL: URL {
-            return URL(string: "https://image.tmdb.org/t/p/w500\(backdrop_path)")!
-    }
-    
-    var posterURL: URL {
-            return URL(string: "https://image.tmdb.org/t/p/w500\(poster_path)")!
-    }
-}
-
 
