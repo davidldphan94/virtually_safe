a8b83ee9b070112f4ec0f77b9043a18ca1b0ebaa
k-kha <kevinkha7@gmail.com>
2021-05-15 20:36:33 -0400

    Added 2 ItemViews + Updated Bank views and ListView

---
+++ virtually_safe/ItemView/DriverView.swift
@@ -0,0 +1,20 @@
+//
+//  DriverView.swift
+//  virtually_safe
+//
+//  Created by Kevin Kha on 5/15/21.
+//
+
+import SwiftUI
+
+struct DriverView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DriverView_Previews: PreviewProvider {
+    static var previews: some View {
+        DriverView()
+    }
+}
